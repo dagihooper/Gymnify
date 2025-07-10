423aeeb033978c4312deaa67eb6c380000a0f04c
Dagmawialebachew <dagmawialebachew@gmail.com>
2025-07-10 16:33:53 +0300

    Add static root for production, and verify the file loading in prodution

--- Gymnify/settings.py
+++ Gymnify/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -178,10 +179,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, "static"),
-        os.path.join(BASE_DIR, "static/assets"),
-
+    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "static/assets"),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # this is added for the production level.
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
